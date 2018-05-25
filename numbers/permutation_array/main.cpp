abde203200d1351f80a98c5fd244fafac536712d
uditgulati <uditgulati0@gmail.com>
2018-05-25 15:40:44 +0530

    Add permutation array

---
+++ numbers/permutation_array/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int lim1_low = 1, lim1_high = 10;
+int lim2_low = 1, lim2_high = 100;
+int lim3_low = 1, lim3_high = 1e9;
+
+int main() {
+	srand(time(NULL));
+
+	int testcases = rand() % (lim1_high - lim1_low + 1) + lim1_low;
+	printf("%d\n", testcases);
+	
+	assert(testcases >= lim1_low && testcases <= lim1_high);
+
+	while(testcases--) {
+		int n = rand() % (lim2_high - lim2_low + 1) + lim2_low;
+		printf("%d\n", n);
+
+		assert(n >= lim2_low && n <= lim2_high);
+
+		int arr[n];
+
+		for(int i = 0; i < n; i++)
+			arr[i] = i + 1;
+		
+		int num = rand() % n;
+
+		while(num--)
+			next_permutation(arr, arr + n);
+
+		vector <int> vec1, vec2;
+
+		for(int i = 0; i < n; i++) {
+			vec1.push_back(i + 1);
+			vec2.push_back(arr[i]);
+		}
+
+		sort(vec2.begin(), vec2.end());
+
+		assert(vec1 == vec2);
+
+		for(int i = 0; i < n; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+	}
+	return 0;
+}
